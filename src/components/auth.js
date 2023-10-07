2d5455034cc30db9c03f39b5be75b168f35a3faa
Prasoon <Prasoon@Prasoons-MacBook-Pro.local>
2023-10-07 15:27:27 +0530

    feat: added google button and integrated signin function

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import Signin from "./signin";
 import Signup from "./signup";
+import GoogleButton from "./googleButton";
+import { useUserContext } from "../context/userContext";
 
 const Auth = () => {
   const [index, setIndex] = useState(false);
   const toggleIndex = () => {
     setIndex((prevState) => !prevState);
   };
+  const { signInWithGoogle } = useUserContext();
+
   return (
     <div className="container">
       {!index ? <Signin /> : <Signup />}
+      <GoogleButton onClick={signInWithGoogle}>
+        Continue with Google
+      </GoogleButton>
       <p onClick={toggleIndex}>
         {!index ? "New user? Click here " : "Already have an acount?"}
       </p>
